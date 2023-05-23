b1284e5f4e6fd392caca8e564295a083ade96530
hassan ali <hassanali5120@gmail.com>
2023-05-23 19:18:17 +0500

    latest update

---
+++ resources/views/PublicPanel/fashion.blade.php
@@ -0,0 +1,80 @@
+@extends('PublicPanel.layout.master')
+@section('content')
+    <!-- Main-Section -->
+    <div class="container-fluid">
+        <div class="container  mt-5 px-5 mx-5 mb-5">
+            <div class="row">
+                <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+                    <img src="PublicAssets/Ecom/DSC02827.jpg" class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Boat on Calm Water" />
+
+                    <img src="PublicAssets/Ecom/DSC02846.jpg" class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Wintry Mountain Landscape" />
+                </div>
+
+                <div class="col-lg-4 mb-4 mb-lg-0">
+                    <img src="PublicAssets/Ecom/DSC06555.jpg" class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Mountains in the Clouds" />
+
+                    <img src="PublicAssets/Ecom/DSCF9694 (1).jpg" class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Boat on Calm Water" />
+                </div>
+
+                <div class="col-lg-4 mb-4 mb-lg-0">
+                    <img src="PublicAssets/Ecom/DSCF9718.jpg" class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Waves at Sea" />
+
+                    <img src="PublicAssets/Ecom/IMG_0017.jpg" class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Yosemite National Park" />
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+                    <img src="PublicAssets/Ecom/IMG_0229.jpg" class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Boat on Calm Water" />
+
+                    <img src="PublicAssets/Ecom/IMG_0301.jpg" class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Wintry Mountain Landscape" />
+                </div>
+
+                <div class="col-lg-4 mb-4 mb-lg-0">
+                    <img src="PublicAssets/Ecom/IMG_0377.jpg" class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Mountains in the Clouds" />
+
+                    <img src="PublicAssets/Ecom/IMG_6481.JPG" class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Boat on Calm Water" />
+                </div>
+
+                <div class="col-lg-4 mb-4 mb-lg-0">
+                    <img src="PublicAssets/Ecom/Screenshot 2023-05-11 at 8.40.30 PM.png" class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Waves at Sea" />
+
+                    <img src="PublicAssets/Ecom/Screenshot 2023-05-11 at 8.41.27 PM.png" class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Yosemite National Park" />
+                </div>
+
+            </div>
+             <div class="row">
+                <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+                    <img src="PublicAssets/Ecom/Screenshot 2023-05-11 at 8.42.08 PM.png" class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Boat on Calm Water" />
+
+                    <img src="PublicAssets/Ecom/DSC02846.jpg" class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Wintry Mountain Landscape" />
+                </div>
+
+                <div class="col-lg-4 mb-4 mb-lg-0">
+                    <img src="PublicAssets/Ecom/Screenshot 2023-05-11 at 8.42.24 PM.png" class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Mountains in the Clouds" />
+
+                    <img src="PublicAssets/Ecom/Screenshot 2023-05-11 at 8.45.35 PM.png" class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Boat on Calm Water" />
+                </div>
+
+             
+
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/PublicPanel/flims.blade.php
@@ -0,0 +1,37 @@
+@extends('PublicPanel.layout.master')
+@section('content')
+    <!-- Main-Section -->
+    <div class="container-fluid">
+        <div class="container  mt-5 px-5 mx-5 mb-5">
+            <div class="row">
+
+                <div class="col-6 mx-8"><iframe width="560" height="315" src="https://www.youtube.com/embed/O7jS5PZdzjc"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowfullscreen></iframe>
+                </div>
+                <div class="col-6 mx-8"><iframe width="560" height="315" src="https://www.youtube.com/embed/M7_juRHINNI"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowfullscreen></iframe>
+                </div>
+                <div class="col-6 mx-8"><iframe width="560" height="315" src="https://www.youtube.com/embed/30baPA3dnMY"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowfullscreen></iframe>
+                </div>
+                <div class="col-6 mx-8"><iframe width="560" height="315" src="https://www.youtube.com/embed/Zy5xgIZh5Xs"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowfullscreen></iframe>
+                </div>
+                <div class="col-6 mx-8">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/cGd4LOAG4HU"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
